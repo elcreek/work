5bc52015aed36127a87288117bfac70567661c64
wafike <wafike@gmail.com>
2022-01-28 11:45:24 +0100

    j

--- ccxt_data_fetch.py
+++ ccxt_data_fetch.py
@@ -2,43 +2,56 @@ import ccxt
 import os
 import pandas as pd
 from datetime import datetime, timedelta
-
+from tqdm import tqdm
+from functools import partial
+from pathlib import Path
 
 
 exchange = ccxt.binance()
 
-# exchange = ccxt.binance({
-#     'enableRateLimit': True,
-# })
-
-
-# exchange.enableRateLimit = True 
 exchange.load_markets()
 
 symbols = exchange.symbols
 
 
-# ticker = 'BTC-USDT'
-# timeframe ='4h'
-# use bars[:-1] to avoid false signal espeacially in current candle
-# because ccxt gives the ongoing candle before they close
-# do bars[:-3] to get less trades; but better quality
-def ccxt_data(symbol='ETH/USDT', timeframe ='4h', limit=111):
-    global data
-    # global fullname
-    # i use globl for data variable even thogh the function returns data variable
-    # because jupyter cells are ceperated from each other
+t = 'USDT'
+pairs = []
+# global unwanted
+unwanted = ["GYEN","UP/","DOWN/","USDS","USDT/","USDSB","SUSD",
+"NCASH/ETH","DNT/ETH","BCN/ETH","UST","ETH/","BKRW","VAI","RUB",
+"NGN","DAI","BIDR","BEAR", "BULL", "PAX", "TUSD", "UMA", "USDC",
+ "USDP", "BUSD/", "EUR", "GBP", "TRY", "AUD","BRL", "BVND"]
+
+for s in symbols:
+    if (t in s):
+        pairs.append(s)
+
+pairs=[n for n in pairs if all(m not in n for m in unwanted)]
+
+
+_pairs = ["ETH/USDT","ATOM/USDT"]
+
+# always put the list out of the loop or everthing will be wiped out each turn
+order=5
+timeframe = '1d'
+limit = 111
+rows = []
+td =datetime.now().strftime('%Y-%m-%d_%H')
+# global tof
+for symbol in tqdm(pairs):
 
     m_symbol = symbol.replace("/","_")
     outname = m_symbol+'_'+timeframe+'_'+f'{limit}'+'.csv'
-    outdir=os.getcwd()+f'/data/{timeframe}'
-    fullname = os.path.join(outdir, outname)    
+
+    outdir=f"{os.getcwd()}/data/{td}/{timeframe}"
+    # outdir=f"{os.getenv('HOME')}/data/{td}/{timeframe}"
+    fullname = os.path.join(outdir, outname) 
+   
 
     if not os.path.exists(outdir):
-        os.mkdir(outdir)
+        os.makedirs(outdir)
 
     if not (os.path.exists(fullname)):
-
         bars = exchange.fetch_ohlcv(symbol, timeframe=timeframe, limit=limit)
         # must use bars[:-1] because arrgrelextrema will see the last candle wich have not closed yet
         data = pd.DataFrame(bars[:], columns=['Time', 'Open', 'High', 'Low', 'Close', 'Volume'])
@@ -46,38 +59,9 @@ def ccxt_data(symbol='ETH/USDT', timeframe ='4h', limit=111):
         data.set_index('Time', inplace=True)
 
         data.to_csv(fullname) 
-        print('fetching new data', symbol)
+        # print('fetching new data', symbol)
     
-    else:
-        tem_data = pd.read_csv(fullname, index_col='Time')
-        # last_candle_time=tem_data['Time'].max()
-        # print(type(tem_data.index[-1]))
-        # c =pd.to_datetime(tem_data.index[-1])
-        # d =  c- datetime.now()
-        # print(c, d)
-        if tem_data.empty:
-            os.remove(fullname)
 
-        if pd.to_datetime(tem_data.index[-1]) < (pd.to_datetime(datetime.now()) - pd.Timedelta(8, unit="h")):
-            os.remove(fullname)
-        
-        elif pd.to_datetime(tem_data.index[-1]) + pd.Timedelta(4, unit="h") > pd.to_datetime(datetime.now()):
-            data = tem_data
-        # ll=pd.to_datetime(datetime.now()) - pd.Timedelta(4, unit="h")
-
-        # if ll in tem_data.index:
-        #     if ll == tem_data.index[-1]:
-        #         data = tem_data
-        else:
-            bars = exchange.fetch_ohlcv(symbol, timeframe=timeframe, limit=limit)
-            data = pd.DataFrame(bars[:], columns=['Time', 'Open', 'High', 'Low', 'Close', 'Volume'])
-            data['Time'] = pd.to_datetime(data['Time'], unit='ms')
-            data.set_index('Time', inplace=True)
-
-            data.to_csv(fullname)
-            print('updating data ',symbol)
+ 
+  
             
-   
-    return data
-
-# ccxt_data()
